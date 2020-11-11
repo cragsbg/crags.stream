a07bc8cb071837eb7e23302058bc6873bf4eb856
cragsbg <69464727+cragsbg@users.noreply.github.com>
2020-11-11 11:48:06 +0200

    Feature/fix search (#19)
    * Fix search main.
    
    * Fix search main 2.
    
    * Fix styles.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,60 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let searchResults = [];
-    const searchWrapper = document.querySelector("aside[role=search]");
-    const searchResultElement = searchWrapper.querySelector(".search-results");
-    const searchInput = searchWrapper.querySelector("input");
 
-    document.querySelectorAll(".toggle-search").forEach(function (el) {
-        el.addEventListener("click", function (e) {
-            if (searchWrapper.classList.contains("active")) {
-                searchWrapper.classList.add("visible");
-                setTimeout(function () {
-                    searchWrapper.classList.remove("visible");
-                }, 300);
-                searchWrapper.classList.remove("active");
-            } else {
-                searchWrapper.classList.add("active");
-                searchInput.focus();
-            }
-        });
-    });
+    // getting all required elements
+    const searchWrapper = document.querySelector(".search-input");
+    const inputBox = searchWrapper.querySelector("input");
+    const suggBox = searchWrapper.querySelector(".autocom-box");
+    const icon = searchWrapper.querySelector(".icon");
+    let linkTag = searchWrapper.querySelector("a");
+    let webLink;
 
-    function tags(tags, searchString) {
-        let tagHTML = (tags.split(" ; ") || [])
-            .filter(function (i) {
-                return i && i.length > 0;
-            })
-            .map(function (i) {
-                return "<span class='tag'>" + mark(i, searchString) + "</span>";
-            })
-        return tagHTML.join("");
-    }
-
-    function mark(content, search) {
-        if (search) {
-            let pattern = /^[a-zA-ZА-Яа-я0-9]*:/i;
-            search.split(" ").forEach(function (s) {
-                if (pattern.test(s)) {
-                    s = s.replace(pattern, "");
-                }
-                if (s && s.startsWith("+")) {
-                    s = s.substring(1);
-                }
-                if (s && s.indexOf("~") > 0 && s.length > s.indexOf("~") && parseInt(s.substring(s.indexOf("~") + 1)) == s.substring(s.indexOf("~") + 1)) {
-                    s = s.substring(0, s.indexOf("~"));
-                }
-                if (!s || s.startsWith("-")) {
-                    return;
-                }
-                let re = new RegExp(s, "i");
-                content = content.replace(re, function (m) {
-                    return "<mark>"+m+"</mark>";
-                });
-            });
+    function showSuggestions(list) {
+        let listData;
+        if (!list.length) {
+            userValue = inputBox.value;
+            listData = '<li>' + userValue + '</li>';
+        } else {
+            listData = list.join('');
         }
-
-        return content;
+        suggBox.innerHTML = listData;
     }
+
+    inputBox.value = "";
 
     axios.get("/search")
         .then(function (result) {
@@ -70,42 +35,33 @@ document.addEventListener("DOMContentLoaded", function () {
                 Array.from(result.data).forEach(function (doc) {
                     this.add(doc)
                 }, this)
-            })
-            searchInput.removeAttribute("disabled");
-            searchInput.addEventListener("keyup", function (e) {
-                let searchString = e.target.value;
-                if (searchString && searchString.length > 2) {
+            });
+            // if user press any key and release
+            inputBox.onkeyup = (e) => {
+                searchWrapper.scrollIntoView();
+                let userData = e.target.value; //user enetered data
+                let emptyArray = [];
+                if (userData) {
                     try {
-                        searchResults = searchIndex.search(searchString);
+                        emptyArray = searchIndex.search(userData);
                     } catch (err) {
                         if (err instanceof lunr.QueryParseError) {
                             return;
                         }
                     }
-                } else {
-                    searchResults = [];
-                }
-
-                if (searchResults.length > 0) {
-                    searchResultElement.innerHTML = searchResults.map(function (match) {
-                        let item = searchContent.find(function(e) {
-                            return e.id == parseInt(match.ref);
+                    emptyArray = emptyArray.map(data => {
+                        let item = searchContent.find(function (e) {
+                            return e.id == parseInt(data.ref);
                         });
-                        return "<li>" +
-                            "<h4 title='field: title'><a href='" + item.url + "'>" + mark(item.title, searchString) + "</a></h4>" +
-                            "<p class='summary' title='field: content'>" +
-                            mark((item.content.length > 200 ? (item.content.substring(0, 200) + "...") : item.content), searchString) +
-                            "</p>" +
-                            "<p class='tags' title='field: tag'>" + tags(item.tag, searchString) + "</p>" +
-                            "<a href='" + item.url + "' title='field: url'>" + mark(item.url, searchString) + "</a>" +
-                            "</li>";
-                    }).join("");
-                } else if (searchString == undefined || searchString.length < 2) {
-                    searchResultElement.innerHTML = "";
+                        // passing return data inside li tag
+                        return '<li onclick="window.location.href=\'' + item.url + '\';">' + item.title + '</li>';
+                    });
+                    searchWrapper.classList.add("active"); //show autocomplete box
+                    showSuggestions(emptyArray);
                 } else {
-                    searchResultElement.innerHTML = "<li><p class='no-result'>Няма намерени резултати</p></li>";
+                    searchWrapper.classList.remove("active"); //hide autocomplete box
                 }
-            });
+            }
         })
         .catch(function (error) {
             console.error(error);
