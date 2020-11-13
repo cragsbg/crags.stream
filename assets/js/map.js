ab96eb71b86e7bf3388f040eac1df506fd0385e4
cragsbg <69464727+cragsbg@users.noreply.github.com>
2020-11-13 08:34:46 +0200

    Feature/make maps better (#21)
    * Rearrange some maps.
    
    * Make maps better - fix js.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let mymap = L.map('map');
 
-    L.tileLayer('https://bgmtile.kade.si/{z}/{x}/{y}.png', {
+    var bgmTileLayer = L.tileLayer('https://bgmtile.kade.si/{z}/{x}/{y}.png', {
         attribution: '<a href="https://web.uni-plovdiv.net/vedrin/bgm/BGMountains_Legend.jpg" target="_blank">BGM Legend</a> | <a href="https://cart.uni-plovdiv.net/" target="_blank">CART Lab</a> | <a href="http://www.bgmountains.org/" target="_blank">BGM team</a> | Tiles © <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0</a>',
         maxZoom: 50
-    }).addTo(mymap);
+    });
 
-    function drawTrack(track) {
-        let coordinates = track.points.map(p => [p.lat.toFixed(5), p.lon.toFixed(5)]);
+    var osmTileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: ['a', 'b', 'c']
+    });
 
-        var polyline = L.polyline(coordinates, { weight: 6, color: 'darkred' }).addTo(mymap);
+    let myMap = L.map('map', {
+        layers: [osmTileLayer, bgmTileLayer]
+    });
 
-        // zoom the map to the polyline
-        mymap.fitBounds(polyline.getBounds());
-    }
+    var baseMaps = {
+        "Open Street Map": osmTileLayer,
+        "BG Mountains Map": bgmTileLayer
+    };
+
+    L.control.layers(baseMaps).addTo(myMap);
 
     var trackPath = document.getElementById("map").getAttribute("data-gpx-file");
+    var trackIndex = document.getElementById("map").getAttribute("data-gpx-track-index");
 
     fetch(trackPath)
         .then(function (response) {
@@ -24,17 +31,25 @@ document.addEventListener("DOMContentLoaded", function () {
             let gpx = new gpxParser();
             gpx.parse(gpxData);
 
-            document.getElementById("track-name").innerText = gpx.tracks[0].name;
+            document.getElementById("track-name").innerText = gpx.tracks[trackIndex].name;
 
-            document.getElementById("totalDistance").innerText = (gpx.tracks[0].distance.total / 1000).toFixed(3);
-            document.getElementById("totalDistanceNM").innerText = (gpx.tracks[0].distance.total / 1000 * 0.539956803).toFixed(3);
+            document.getElementById("totalDistance").innerText = (gpx.tracks[trackIndex].distance.total / 1000).toFixed(3);
+            document.getElementById("totalDistanceNM").innerText = (gpx.tracks[trackIndex].distance.total / 1000 * 0.539956803).toFixed(3);
 
             document.getElementById("gpxDownload").innerText = trackPath;
             document.getElementById("gpxDownload").setAttribute("href", trackPath);
 
-            gpx.tracks.forEach(element => {
-                drawTrack(element);
+            // draw track
+            let coordinates = gpx.tracks[trackIndex].points.map(p => [p.lat.toFixed(5), p.lon.toFixed(5)]);
+            var polyline = L.polyline(coordinates, { weight: 6, color: 'darkred' }).addTo(myMap);
+            // zoom the map to the polyline
+            myMap.fitBounds(polyline.getBounds());
+
+            // draw waypoints
+            gpx.waypoints.forEach(waypoint => {
+                L.marker([waypoint.lat, waypoint.lon])
+                    .bindPopup(waypoint.name)
+                    .addTo(myMap);
             });
-            //drawTrack(gpx.tracks[0]);
         });
 });
